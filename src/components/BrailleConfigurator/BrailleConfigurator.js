64fdca52a5db74258cef0966b1828ba3dd430a57
vankrav <vankrav@vankravs-MacBook-Pro.local>
2025-05-08 16:44:54 +0300

    панель настроек брайлля

--- src/components/BrailleConfigurator/BrailleConfigurator.js
+++ src/components/BrailleConfigurator/BrailleConfigurator.js
@@ -2,7 +2,9 @@ import { Card } from "@gravity-ui/uikit";
 import { Row } from "@gravity-ui/uikit";
 import { Col } from "@gravity-ui/uikit";
 import { TextArea } from "@gravity-ui/uikit";
-
+import { NumberInput } from "@gravity-ui/uikit";
+import { Text } from "@gravity-ui/uikit";
+import { Button } from "@gravity-ui/uikit";
 const BrailleConfigurator = () => {
   return (
     <>
@@ -19,20 +21,37 @@ const BrailleConfigurator = () => {
               3D-модель
             </div>
           </Card>
-          <TextArea  style={{ marginTop: "10px", height: "100px" }}></TextArea>
+          <TextArea placeholder="Введите текст" style={{ marginTop: "10px", height: "100px" }}></TextArea>
         </Col>
        
         <Col s="4" space="2">
-        
             <Card type="container" view="filled" style={{ height: "100%" }}>
-            <div style={{ 
-              display: 'flex', 
-              justifyContent: 'center', 
-              alignItems: 'center', 
-              height: '100%' 
-            }}>
-              Настройки
-            </div>
+              <div style={{ 
+                padding: "24px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "20px",
+                height: "100%"
+              }}>
+                <Text variant="header-1">Настройки</Text>
+                
+                <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+                  <Text variant="subheader-2">Размеры</Text>
+                  <NumberInput label="Ширина (мм):" placeholder="Введите ширину" min={10} max={3000} /> 
+                  <NumberInput label="Высота (мм):" placeholder="Введите высоту" min={10} max={3000} /> 
+                  <NumberInput label="Толщина (мм):" placeholder="Введите толщину" min={10} max={3000} /> 
+                </div>
+                
+                <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+                  <Text variant="subheader-2">Отступы</Text>
+                  <NumberInput label="Отступ сверху (мм):" placeholder="Введите отступ сверху" min={10} max={3000} /> 
+                  <NumberInput label="Отступ сбоку (мм):" placeholder="Введите отступ сбоку" min={10} max={3000} /> 
+                </div>
+
+                <div style={{ marginTop: "auto" }}>
+                  <Button size="xl" view="action" width="max">Экспорт модели</Button>
+                </div>
+              </div>
             </Card>
         </Col>
       </Row>
